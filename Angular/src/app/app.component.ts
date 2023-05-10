57f2179057828589a8ebfef0feed9a9e44a90d15
OreBoia <e99delsarto@gmail.com>
2023-05-10 16:48:23 +0200

    elementi condizionali

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Angular'
+
+  isVisible = true
+
+  persone = [
+    {nome: 'Luca', cognome: 'rossi', isOnline: true},
+    {nome: 'Marco', cognome: 'Verdi', isOnline: false},
+    {nome: 'Anna', cognome: 'neri', isOnline: false},
+    {nome: 'pippo', cognome: 'paperino', isOnline: true}]
   
-  onClick(e: any){console.log(e)}
-  onInput(event: Event){console.log((<HTMLInputElement>event.target).value)}
+
+  onClick(event: Event){this.title = 'HOCLICCATO'}
+  onInput(event: Event){this.title = (<HTMLInputElement>event.target).value}
 }
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms'
 
 import { MatSliderModule } from '@angular/material/slider'
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import {MatInputModule} from '@angular/material/input';
     MatSliderModule,
     MatCardModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
